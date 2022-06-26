3c3d25d7e5cc9c3b0e0897180abb3915644ce547
Bmattholder <bmattholder@gmail.com>
2022-06-25 21:45:44 -0500

    handlers

--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"./handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request){
-		log.Println("Hello World!")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s\n", d)
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
-		log.Println("Goodbye World!")
-	})
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
-	http.ListenAndServe(":9090", nil)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	http.ListenAndServe(":9090", sm)
 }
